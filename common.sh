acac4db04664c85e44c5362fc4451b3aa9b4dd7a
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-05-28 20:40:30 +0530

    changes

--- common.sh
+++ common.sh
@@ -2,5 +2,5 @@ log_file=/tmp/expense.log
 
 download_and_extract() {
     curl -sL -o /tmp/$component.zip https://expense-artifacts.s3.amazonaws.com/$component.zip   &>>$log_file
-    unzip /tmp/$component.zip  &>$log_file>
+    unzip /tmp/$component.zip  &>$log_file
 }
