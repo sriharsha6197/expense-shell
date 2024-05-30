1e29f0896b35bc10b0b6105ccc210735ca95b0dd
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-05-30 13:49:09 +0530

    passwordless

--- mysql.sh
+++ mysql.sh
@@ -1,5 +1,5 @@
 source common.sh
-
+mysql_password=$1
 echo disabling mysql
 dnf module disable mysql -y &>>log_file
 exit_status_check
@@ -18,9 +18,9 @@ systemctl start mysqld  &>>log_file
 exit_status_check
 
 echo checking secure connnection
-mysql_secure_installation --set-root-pass SriHarsha@1  &>>log_file
+mysql_secure_installation --set-root-pass $mysql_password  &>>log_file
 exit_status_check
 
 echo connecting to the database
-mysql -uroot -pSriHarsha@1  &>>log_file
+mysql -uroot -p$mysql_password  &>>log_file
 exit_status_check
